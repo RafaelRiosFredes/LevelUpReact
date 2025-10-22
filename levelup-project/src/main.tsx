a7c1e4c38771badfdb8e35c46ebf0c631e21ff02
FranciscaA <fr.arancibiac@duocuc.cl>
2025-10-22 11:21:31 -0300

    Creacion de pagina principal

--- levelup-project/src/main.tsx
+++ levelup-project/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Navbar } from './components/NavBar.tsx'
+import { Home } from './pages/Home.tsx'
+import { Footer } from './components/footer.tsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./assets/styles.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Navbar />
+    <Home />
+    <Footer/>
+  
   </StrictMode>,
 )
