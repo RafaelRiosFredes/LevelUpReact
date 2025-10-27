4ffd3843182762e6280ecf9d7e8fd637e6ab27af
RafaelRiosFredes <raf.rios@duocuc.cl>
2025-10-27 13:29:14 -0300

    Resolviendo bugs de funcionalidad

--- levelup-project/src/App.tsx
+++ levelup-project/src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./assets/styles.css";
+import { Routes, Route } from "react-router-dom";
 
 import { DetalleProducto } from "./pages/DetalleProducto";
 import { NavBar } from "./components/NavBar";
@@ -9,13 +9,13 @@ import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
+    <> {/* Usamos un Fragmento React para agrupar los elementos */}
       <NavBar />
       <Routes>
         <Route path="/detalle" element={<DetalleProducto />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
--- levelup-project/src/main.tsx
+++ levelup-project/src/main.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
+import { CartProvider } from "./context/CartContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./assets/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* 2. Envolver la aplicación con CartProvider */}
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
